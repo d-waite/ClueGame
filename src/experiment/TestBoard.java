2f7624b2addf12bc764d5225642e7f887e9fd942
David Waite <davidwaite@mines.edu>
2022-02-26 11:35:32 -0700

    Created code stubs for testing

---
+++ src/experiment/TestBoard.java
@@ -0,0 +1,24 @@
+package experiment;
+
+import java.util.*;
+
+public class TestBoard {
+
+	public TestBoard() {
+		
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		Set<TestBoardCell> empty = new HashSet<TestBoardCell>();
+		return empty;
+	}
+	
+	public TestBoardCell getCell( int row, int col ) {
+		TestBoardCell cell = new TestBoardCell(-1,-1);
+		return cell;
+	}
+}
---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,34 @@
+package experiment;
+import java.util.*;
+
+public class TestBoardCell {
+
+	public TestBoardCell(int row, int column) {
+		
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+	public Set<TestBoardCell> getAdjList() {
+		Set<TestBoardCell> empty = new HashSet<TestBoardCell>();
+		return empty;
+	}
+	
+	public void setRoom(boolean room) {
+		
+	}
+	
+//	public boolean isRoom() {
+//		
+//	}
+	
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+//	public boolean getOccupied() {
+//		
+//	}
+}
