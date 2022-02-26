ca16602d945f6058ce8c85c1190a85a65cd78db4
Dillinger Day <djday@mines.edu>
2022-02-26 12:08:48 -0700

    Created test cases for the creation of adjacency lists for a 4x4 board.

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -2,9 +2,11 @@ package experiment;
 import java.util.*;
 
 public class TestBoardCell {
-
+	private int row;
+	private int column;
 	public TestBoardCell(int row, int column) {
-		
+		this.row = row;
+		this.column = column;
 	}
 	
 	public void addAdjacency(TestBoardCell cell) {
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -2,13 +2,73 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import experiment.TestBoard;
+import experiment.TestBoardCell;
 
 class BoardTestsExp {
 
+	private TestBoard board;
+	@BeforeEach
+	public void setup() {
+		board = new TestBoard();
+		
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void adjListTopLeft() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+	
+	@Test
+	public void adjListBottomRight() {
+		TestBoardCell cell = board.getCell(3, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(3, 2)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
+		assertEquals(2, testList.size());
+	}
+	@Test
+	public void adjListLeftEdge() {
+		TestBoardCell cell = board.getCell(2, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(3, 0)));
+		assertTrue(testList.contains(board.getCell(2, 1)));
+		assertEquals(3, testList.size());
 	}
 
+	@Test
+	public void adjListRightEdge() {
+		TestBoardCell cell = board.getCell(2, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(3, 3)));
+		assertTrue(testList.contains(board.getCell(1, 3)));
+		assertTrue(testList.contains(board.getCell(2, 2)));
+		assertEquals(3, testList.size());
+	}
+	
+	@Test
+	public void adjListMiddle() {
+		TestBoardCell cell = board.getCell(1, 2);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		assertTrue(testList.contains(board.getCell(2, 2)));
+		assertTrue(testList.contains(board.getCell(1, 3)));
+		assertTrue(testList.contains(board.getCell(0, 2)));
+		assertTrue(testList.contains(board.getCell(1, 1)));
+		assertEquals(4, testList.size());
+	}
+
+	@Test
+	public void 
 }
