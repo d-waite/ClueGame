5803cd73b26523041b5ad331cfef0602bf95933e
Dillinger Day <djday@mines.edu>
2022-04-08 15:50:32 -0600

    Got update panel functions to work.

--- src/clueGame/GameCardPanel.java
+++ src/clueGame/GameCardPanel.java
@@ -1,31 +1,38 @@
 package clueGame;
 
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class GameCardPanel extends JPanel {
-
+	private JPanel peopleCards;
+	private JPanel roomCards;
+	private JPanel weaponCards;
+	
 	public GameCardPanel() {
 		setLayout(new GridLayout(3,1));
 		setBorder(new TitledBorder(new EtchedBorder(), "Known Cards"));
-		JPanel peopleCards = new JPanel();
+		
+		peopleCards = new JPanel();
 		peopleCards.setBorder(new TitledBorder(new EtchedBorder(), "People"));
 		peopleCards.setLayout(new GridLayout(0,1));
 		peopleCards.add(createInHandLabel());
 		peopleCards.add(createNoneField());
 		peopleCards.add(createSeenLabel());
 		peopleCards.add(createNoneField());
-		JPanel roomCards = new JPanel();
+		
+		roomCards = new JPanel();
 		roomCards.setBorder(new TitledBorder(new EtchedBorder(), "Rooms"));
 		roomCards.setLayout(new GridLayout(0,1));
 		roomCards.add(createInHandLabel());
 		roomCards.add(createNoneField());
 		roomCards.add(createSeenLabel());
 		roomCards.add(createNoneField());
-		JPanel weaponCards = new JPanel();
+		
+		weaponCards = new JPanel();
 		weaponCards.setBorder(new TitledBorder(new EtchedBorder(), "Weapons"));
 		weaponCards.setLayout(new GridLayout(0,1));
 		weaponCards.add(createInHandLabel());
@@ -45,6 +52,7 @@ public class GameCardPanel extends JPanel {
 	
 	public JTextField newKnownCard(Card card) {
 		JTextField knownCard = new JTextField(card.getCardName());
+		knownCard.setEditable(false);
 		return knownCard;
 	}
 	
@@ -56,7 +64,63 @@ public class GameCardPanel extends JPanel {
 		return new JLabel("Seen:");
 	}
 	
+	public void updatePeopleCards(ArrayList<Card> seenList, ArrayList<Card> hand) {
+		peopleCards.removeAll();
+		peopleCards.add(createInHandLabel());
+		for (Card card: hand) {
+			if (card.getCardType().equals(CardType.PERSON)) {
+				peopleCards.add(newKnownCard(card));
+			}
+		}
+		peopleCards.add(createSeenLabel());
+		for (Card card: seenList) {
+			if (card.getCardType().equals(CardType.PERSON)) {
+				peopleCards.add(newKnownCard(card));
+			}
+		}
+	}
+	
+	public void updateRoomCards(ArrayList<Card> seenList, ArrayList<Card> hand) {
+		roomCards.removeAll();
+		roomCards.add(createInHandLabel());
+		for (Card card: hand) {
+			if (card.getCardType().equals(CardType.ROOM)) {
+				roomCards.add(newKnownCard(card));
+			}
+		}
+		roomCards.add(createSeenLabel());
+		for (Card card: seenList) {
+			if (card.getCardType().equals(CardType.ROOM)) {
+				roomCards.add(newKnownCard(card));
+			}
+		}
+	}
+	
+	public void updateWeaponCards(ArrayList<Card> seenList, ArrayList<Card> hand) {
+		weaponCards.removeAll();
+		weaponCards.add(createInHandLabel());
+		for (Card card: hand) {
+			if (card.getCardType().equals(CardType.WEAPON)) {
+				weaponCards.add(newKnownCard(card));
+			}
+		}
+		weaponCards.add(createSeenLabel());
+		for (Card card: seenList) {
+			if (card.getCardType().equals(CardType.WEAPON)) {
+				weaponCards.add(newKnownCard(card));
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
+		ArrayList<Card> hand = new ArrayList<Card>();
+		ArrayList<Card> seen = new ArrayList<Card>();
+		hand.add(new Card("A", CardType.PERSON));
+		hand.add(new Card("C", CardType.ROOM));
+		hand.add(new Card("E", CardType.WEAPON));
+		seen.add(new Card("B", CardType.PERSON));
+		seen.add(new Card("D", CardType.ROOM));
+		seen.add(new Card("F", CardType.WEAPON));
 		GameCardPanel cardDisplay = new GameCardPanel();
 		JFrame clueFrame = new JFrame();
 		clueFrame.setContentPane(cardDisplay);
@@ -64,5 +128,8 @@ public class GameCardPanel extends JPanel {
 		clueFrame.setTitle("Clue--Medieval Theme");
 		clueFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		clueFrame.setVisible(true);
+		cardDisplay.updatePeopleCards(seen, hand);
+		cardDisplay.updateRoomCards(seen, hand);
+		cardDisplay.updateWeaponCards(seen, hand);
 	}
 }
